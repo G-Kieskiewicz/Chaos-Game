e4f86ec689fad1bec59ca4ec6b5d7bcb8bb75a8a
Gabriel <kieskiewiczG@icloud.com>
2026-09-08 12:16:31 +0100

    Update updated.py
    updating it to follow proper principles but it doesnt work for now

--- updated.py
+++ updated.py
@@ -1,54 +1,60 @@
-import numpy as np
+import pyvista as pv
 import matplotlib.pyplot as plt
 import math
 import random
 
-theta = 0
-n = 4
-h = 1
-verticies = np.zeros((n,2) , dtype = 'float')
+history = [0,0,0] # index 0 = current point, index 1 = previous point, index 2 = 2 points before current
 
-iterationCount = 10000
-points = np.zeros((iterationCount,2), dtype = 'float')
-r = 0.5
+# let width == height so we take in length
+def initialise_polygon(length, n):
+    d_theta = (2*math.pi)/n
+    radius = length/2 -10
+    points = []
 
-for i in range(n):
-    x = math.cos(theta) * h
-    y = math.sin(theta) * h
-    theta = theta + ((2*math.pi)/n)
-    verticies[i] = [x,y]
+    for i in range(0,n):
+        theta = i * d_theta
+        points.append((radius*math.sin(theta),radius*math.cos(theta)))
+    return points
 
-idx = [0, 0, 0]  # idx[0] = current pick, idx[1] = previous pick, idx[2] = pick before that
-
-def random_point_index():
-    global idx
-    idx[2] = idx[1]
-    idx[1] = idx[0]
-    dst1 = abs(idx[1] - idx[2])
+def random_points(points,history):
+    if len(points) <= 3:
+        return random.randint(0,len(points)-1)
+    history[2] = history[1]
+    history[1] = history[0]
+    distance1 = abs(history[1] - history[2])
 
     while True:
-        idx[0] = random.randint(0, n - 1)
-        dst = abs(idx[0] - idx[1])
-        if dst1 == 0 and (dst == 1 or dst == n - 1):
+        history[0] = random.randint(0, len(points) - 1)
+        distance_2 = abs(history[0] - history[1])
+        if distance1 == 0 and (distance_2 == 1 or distance_2 ==  len(points) - 1):
             continue
         else:
             break
+    return history[0]
 
-    return idx[0]
+"""test = random_points([1,2,3,4,5],history)
+print(test)"""
 
-for i in range(iterationCount-1):
-    index = random_point_index()
-    point_1 = verticies[index]
-    point_2 = points[i]
+def main():
+    print("=== Chaos Game ===\n")
+    n = int(input("Enter number of verticies, n (>=3): "))
+    r = float(input("Enter the modifier between 0 and 1, r (0...1)"))
+    print("\nGenerating samples... please wait...")
 
-    x = point_2[0] + (point_1[0] - point_2[0]) * r
-    y = point_2[1] + (point_1[1] - point_2[1]) * r
+    length = 100
+    points = initialise_polygon(length, n)
 
-    points[i+1] = [x,y]
+    for i in range(n,100000):
+        idx = random_points(points, history)
+        point_1 = points[idx]
+        point_2 = points[i]
 
-fig, ax = plt.subplots()
-ax.scatter(verticies[:,0], verticies[:,1])
-ax.scatter(points[:,0], points[:,1], s=1, marker='.')
-ax.set_aspect('equal')
-ax.grid(True)
-plt.show()
+        x = point_2[0] + (point_1[0] - point_2[0]) * r
+        y = point_2[1] + (point_1[1] - point_2[1]) * r
+
+        points[i+1].append((x,y))
+
+    print(points)
+
+if __name__ == "__main__":
+    main()
